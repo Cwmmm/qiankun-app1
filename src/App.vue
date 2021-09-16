be62388695606a3d5c5b85a44c15f84256272e99
cwm <cwmmm5277@gmail.com>
2021-09-16 16:55:43 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+      这里是app1
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style>
+#app {
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,55 @@
+import './public-path';
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+import routes from './router';
+import store from './store';
+
+Vue.config.productionTip = false;
+
+let router = null;
+let instance = null;
+function render(props = {}) {
+  const { container } = props;
+  router = new VueRouter({
+    base: window.__POWERED_BY_QIANKUN__ ? '#/app1/' : '#/',
+    mode: 'history',
+    routes,
+  });
+
+  instance = new Vue({
+    router,
+    store,
+    render: (h) => h(App),
+  }).$mount(container ? container.querySelector('#app') : '#app');
+}
+
+// 独立运行时
+if (!window.__POWERED_BY_QIANKUN__) {
+  render();
+}
+
+export async function bootstrap() {
+  console.log('[vue] vue app bootstraped');
+}
+export async function mount(props) {
+  const state = {
+    userInfo: {
+      name: 'cwm',
+      pds: '1234567'
+    }
+  }
+  props.onGlobalStateChange((state, prev) => {
+    // state: 变更后的状态; prev 变更前的状态
+    console.log(state, prev);
+  });
+  props.setGlobalState(state);
+
+  render(props);
+}
+export async function unmount() {
+  instance.$destroy();
+  instance.$el.innerHTML = '';
+  instance = null;
+  router = null;
+}

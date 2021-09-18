319a59687acdcbcd4db83674b0b00b7c1512c8c1
cwm <cwmmm5277@gmail.com>
2021-09-18 17:56:14 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import './public-path';
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './App.vue';
-// import routes from './router';
 import store from './store';
 import globalRegister from './store/global-register'
 import Home from "./views/Home.vue";
@@ -13,6 +12,8 @@ Vue.config.productionTip = false;
 let router = null;
 let instance = null;
 let routerBase = null
+
+//挂载
 function render(props = {}) {
   const { container } = props;
   routerBase = props.routerBase ? props.routerBase.substring(1) : ''
@@ -23,21 +24,26 @@ function render(props = {}) {
       {
         path: routerBase,
         name: 'home',
+        meta: {
+          title: '主页'
+        },
         component: Home,
       },
       {
         path: routerBase + "/about",
         name: "about",
+        meta: {
+          title: '关于'
+        },
         component: About
       },
     ],
   });
+  //初始化基座项目左侧菜单
   store.dispatch('global/setGlobalState', {
-    leftMenu: {
-      1: '1234',
-      2: '423'
-    }
+    leftMenu: router.options.routes
   })
+  //挂载dom
   instance = new Vue({
     router,
     store,
@@ -51,12 +57,15 @@ if (!window.__POWERED_BY_QIANKUN__) {
   render();
 }
 
+//提供给基座调用
 export async function bootstrap() {
   console.log('[vue] vue app bootstraped');
 }
+
 export async function mount(props) {
   globalRegister(store, props)
   render(props);
+
 }
 export async function unmount() {
   instance.$destroy();
